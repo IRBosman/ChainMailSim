e4ace36831eacb9e1e473ec332fcd159106bd33f
Bosman Bos <>
2024-03-30 21:11:14 +0100

    Fixes attack matrix

--- Attack matrix.cs
+++ Attack matrix.cs
@@ -13,7 +13,7 @@ namespace ChainmailSim
             //      LF                              HF              AF                      LH                   MH                  HH
             /*LF*/ [new Attacks(1m,6), new Attacks(1/2m, 6), new Attacks(1/3m, 6), new Attacks(1/2m, 6), new Attacks(1/3m, 6), new Attacks(1/4m, 6)],
             /*HF*/ [new Attacks(1m,5), new Attacks(1m, 6),   new Attacks(1/2m, 6), new Attacks(1/2m, 6), new Attacks(1/3m, 6), new Attacks(1/4m, 6)],
-            /*AF*/ [new Attacks(1m,4), new Attacks(1/2m, 5), new Attacks(1/3m, 6), new Attacks(1m, 6),   new Attacks(1/2m, 6), new Attacks(1/3m, 6)],
+            /*AF*/ [new Attacks(1m,4), new Attacks(1/2m, 5), new Attacks(1m, 6),   new Attacks(1m, 6),   new Attacks(1/2m, 6), new Attacks(1/3m, 6)],
             /*LH*/ [new Attacks(2m,5), new Attacks(2m, 6),   new Attacks(1m, 6),   new Attacks(1m, 6),   new Attacks(1/2m, 6), new Attacks(1/3m, 6)],
             /*MH*/ [new Attacks(2m,4), new Attacks(2m, 5),   new Attacks(2m, 6),   new Attacks(1m, 5),   new Attacks(1m, 6),   new Attacks(1m, 6)],
             /*HH*/ [new Attacks(4m,5), new Attacks(3/1m, 5), new Attacks(2/1m, 5), new Attacks(2m, 6),   new Attacks(1/1m, 5), new Attacks(1m, 6)],
